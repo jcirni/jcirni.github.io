6cae427a36ebf64de51e5b322a15c8150d31929e
jcirni <josephcirnigliaro@gmail.com>
2016-03-20 18:02:40 -0400

    Contact form rev

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -45,7 +44,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success').append("<strong>Thank you for contacting me!");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-success').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
