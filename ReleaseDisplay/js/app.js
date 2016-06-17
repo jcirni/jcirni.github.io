051b4a3f60a494dfc0980c5c602914d64c8c8809
jcirni <josephcirnigliaro@gmail.com>
2016-06-17 19:15:52 -0400

    append titles and date/time to DOM

--- ReleaseDisplay/js/app.js
+++ ReleaseDisplay/js/app.js
@@ -4,8 +4,18 @@ var main = function () {
     var url = "http://www.stellarbiotechnologies.com/media/press-releases/json";
     
     $.getJSON(url, function (pressReleases) {
+        
+        
         pressReleases.news.forEach(function (headline) { 
-            console.log(headline.title + ", " + headline.published);
+            var $h2 = $("<h2>");
+            var $p = $("<p>");
+            //console.log(headline.title + ", " + headline.published);
+            $h2.text(headline.title);
+            $p.text(headline.published);
+            $("main .releases").append($h2);
+            $("main .releases").append($p);
+            //$h2.text("");
+            //$p.text("");
         });
         //console.log(pressReleases);
     });
