83af8af515eed1838ac97e4a4c21111a6b85c16e
jcirni <josephcirnigliaro@gmail.com>
2016-06-17 19:53:51 -0400

    add limit & offset params to API call

--- ReleaseDisplay/js/app.js
+++ ReleaseDisplay/js/app.js
@@ -1,14 +1,21 @@
 var main = function () {
     "use strict";
     
-    var url = "http://www.stellarbiotechnologies.com/media/press-releases/json";
+   
     
+    //control api output with limit & offset
+     var limit = 10,
+        offset = 0;
+    
+    var url = "http://www.stellarbiotechnologies.com/media/press-releases/json?limit=" + limit.toString() + "&offset=" + offset.toString();
+    
+    //Call StellBioTech NewsRelease
     $.getJSON(url, function (pressReleases) {
         
         
         pressReleases.news.forEach(function (headline) { 
-            var $h2 = $("<h2>");
-            var $p = $("<p>");
+            var $h2 = $("<h2>"),
+                $p = $("<p>");
             //console.log(headline.title + ", " + headline.published);
             $h2.text(headline.title);
             $p.text(headline.published);
